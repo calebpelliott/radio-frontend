8f91806490035d981d76e7646a8e39a5552fd110
caleb <calebpe531@gmail.com>
2024-08-23 15:35:36 -0400

    add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <>
+    <React.StrictMode>
         <ApolloProvider client={client}>
             <App />
         </ApolloProvider>
-    </>
+    </React.StrictMode>
 );
